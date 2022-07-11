cb57e51c588369d73c76aa7a27b78746c244156a
Oleg <76252495+OlegWirth@users.noreply.github.com>
2022-07-11 14:08:50 +0300

    Update js-1.js

---
+++ js/js-1.js
@@ -0,0 +1,13 @@
+let name1 = prompt('Как вас зовут ?'); 
+document.write ("Меня зовут: " + name1 + " "); 
+let age = prompt('Сколько вам лет ?   ');
+document.write (" Мне" + age + "лет");
+let city = prompt('В каком городе вы живёте ?');
+document.write ("Я проживаю в городе " + city);
+let company = prompt('В какой компании вы работаете ?');
+document.write ("и работаю в компании " + company);
+document.write("Мои контактные данные:")
+let phone = prompt('Какой ваш номер телефона ?');
+document.write ("Телефон: " + phone);
+let email = prompt('Какой ваш почтовый адрес ?');
+document.write ("Почта: " + email);
